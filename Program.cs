fdb8036f4caef76a7a1172a0b3c0b1efc3d65b87
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2021-10-14 16:11:29 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NLog;
+using System.IO;
+using System.Collections.Generic;
+
+namespace movieLibraryAssignment
+{
+    class Program
+    {
+            private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+           UserSelection selections = new UserSelection();
+           selections.MenuSelection();
+        }
+        
+    }
+}
